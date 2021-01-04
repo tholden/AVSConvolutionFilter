c17556ecfeaa61d97a1deef59c6099fd2d7e6ffe
Tom Holden <thomas.holden@gmail.com>
2021-01-04 15:17:03 +0100

    Add archived code

---
+++ resource.h
@@ -0,0 +1,77 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by ape.rc
+//
+#define IDD_CONFIG                      109
+#define IDC_CHECK1                      1000
+#define IDC_EDIT1                       1001
+#define IDC_EDIT2                       1002
+#define IDC_EDIT3                       1003
+#define IDC_EDIT4                       1004
+#define IDC_EDIT5                       1005
+#define IDC_EDIT6                       1006
+#define IDC_EDIT7                       1007
+#define IDC_EDIT8                       1008
+#define IDC_EDIT9                       1009
+#define IDC_EDIT10                      1010
+#define IDC_EDIT11                      1011
+#define IDC_EDIT12                      1012
+#define IDC_EDIT13                      1013
+#define IDC_EDIT14                      1014
+#define IDC_EDIT15                      1015
+#define IDC_EDIT16                      1016
+#define IDC_EDIT17                      1017
+#define IDC_EDIT18                      1018
+#define IDC_EDIT19                      1019
+#define IDC_EDIT20                      1020
+#define IDC_EDIT21                      1021
+#define IDC_EDIT22                      1022
+#define IDC_EDIT23                      1023
+#define IDC_EDIT24                      1024
+#define IDC_EDIT25                      1025
+#define IDC_EDIT26                      1026
+#define IDC_EDIT27                      1027
+#define IDC_EDIT28                      1028
+#define IDC_EDIT29                      1029
+#define IDC_EDIT30                      1030
+#define IDC_EDIT31                      1031
+#define IDC_EDIT32                      1032
+#define IDC_EDIT33                      1033
+#define IDC_EDIT34                      1034
+#define IDC_EDIT35                      1035
+#define IDC_EDIT36                      1036
+#define IDC_EDIT37                      1037
+#define IDC_EDIT38                      1038
+#define IDC_EDIT39                      1039
+#define IDC_EDIT40                      1040
+#define IDC_EDIT41                      1041
+#define IDC_EDIT42                      1042
+#define IDC_EDIT43                      1043
+#define IDC_EDIT44                      1044
+#define IDC_EDIT45                      1045
+#define IDC_EDIT46                      1046
+#define IDC_EDIT47                      1047
+#define IDC_EDIT48                      1048
+#define IDC_EDIT49                      1049
+#define IDC_EDIT50                      1050
+#define IDC_EDIT51                      1051
+#define IDC_EDIT52                      1052
+#define IDC_EDIT53                      1053
+#define IDC_CHECK2                      1054
+#define IDC_CHECK3                      1055
+#define IDC_CHECK4                      1056
+#define IDC_BUTTON1                     1100
+#define IDC_BUTTON2                     1101
+#define IDC_BUTTON3                     1102
+#define IDC_BUTTON4                     1103
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        103
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1061
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
